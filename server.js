46b9dbaedf7b5d2d0b7cb92fe577d8c4de62da20
Karima002 <karima.mouadi@hva.nl>
2025-04-04 15:28:39 +0200

    mappenstructuur

--- server.js
+++ server.js
@@ -6,17 +6,6 @@ import express from 'express'
 import { Liquid } from 'liquidjs';
 
 
-// Vul hier jouw eigen ID in (zie de instructies in de leertaak)
-const personID = 157
-
-// Doe een fetch naar een URL op de WHOIS API, ga pas verder als de fetch gelukt is
-const personResponse = await fetch('https://fdnd.directus.app/items/person/' + personID)
-
-// Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
-const personResponseJSON = await personResponse.json()
-
-personResponseJSON.data.custom = JSON.parse(personResponseJSON.data.custom); // ervoor te zorgen dat de gegevens in personResponseJSON.data.custom (string) in een JSON-object worden omgezet
-
 
 
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
@@ -39,49 +28,53 @@ app.set('views', './views')
 // In je visitekaartje was dit waarschijnlijk index.html
 app.get('/', async function (request, response) {
    // Render index.liquid uit de Views map en geef de opgehaalde data mee, in een variabele genaamd person
-   response.render('index.liquid', {person: personResponseJSON.data}) //Aan die view wordt een object meegegeven. Met de data uit de API, zodat die gegevens ook in die view gebruikt kunnen worden. 
+   response.render('index.liquid',) //Aan die view wordt een object meegegeven. Met de data uit de API, zodat die gegevens ook in die view gebruikt kunnen worden. 
 })
 
 //Nieuwe routes aanmaken voor verschillende sprints
 app.get('/sprint1', async function (request, response) {
-    response.render('sprint1.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint1.liquid') 
  })
 
  app.get('/sprint2', async function (request, response) {
-    response.render('sprint2.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint2.liquid') 
  })
 
  app.get('/sprint3', async function (request, response) {
-    response.render('sprint3.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint3.liquid') 
  })
 
  app.get('/sprint4', async function (request, response) {
-    response.render('sprint4.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint4.liquid') 
  })
 
  app.get('/sprint5', async function (request, response) {
-    response.render('sprint5.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint5.liquid') 
  })
 
  app.get('/sprint6', async function (request, response) {
-    response.render('sprint6.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint6.liquid') 
  })
 
  app.get('/sprint7', async function (request, response) {
-    response.render('sprint7.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint7.liquid') 
  })
 
  app.get('/sprint8', async function (request, response) {
-    response.render('sprint8.liquid', {person: personResponseJSON.data}) 
+    response.render('sprint8.liquid') 
  })
 
  app.get('/experimenten', async function (request, response) {
-   response.render('experimenten.liquid', {person: personResponseJSON.data}) 
+   response.render('experimenten.liquid') 
 })
 
-app.get('/core', async function (request, response) {
-   response.render('core.liquid', {person: personResponseJSON.data}) 
+app.get('/weloveweb', async function (request, response) {
+   response.render('weloveweb.liquid') 
 })
+app.get('/core', async function (request, response) {
+   response.render('core.liquid') 
+})
+
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/', async function (request, response) {
