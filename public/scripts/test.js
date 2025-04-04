b7574fc0dd7eec5ec406ca653a15822ac2214016
Karima002 <karima.mouadi@hva.nl>
2025-04-04 14:04:00 +0200

    #38 kleine change in package.json

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,13 +1,20 @@
-let buttonaudio = document.querySelector ('button')
+let buttonaudio = document.querySelector ('button') //hiermee willen we het geluid afspelen core functionaliteit
 let audio = document.querySelector ('audio')
 
 buttonaudio.addEventListener ('click')
 
-function () {
-    if (buttonaudio.classList.contains('paused')) {
+function buttonaudio () {
+    if (buttonaudio.classList.contains('play')) {
         audio.play()
     } else {
         audio.pause()
     }
 
 }
+
+
+//hiermee verberg je de audio en toon je de button, kan ook met html maar dan heb je altijd javascript nodig om het audio te laten afspelen
+
+
+audio.hidden = true;
+buttonaudio.hidden = false;
